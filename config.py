156b384d46522febbb169112afd68c788cf6a6cd
Emiting Stars <jeffymoses123@gmail.com>
2025-02-01 00:21:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = env.get("BOT_TOKEN", "7560586869:AAG7uvxJQXURcatwG92E38kq_zEsDJ7WIdQ") #Add Bot Token, get from botfather
     FSUB_ID = int(env.get("FSUB_ID", "-1002410513772"))  #Add Your FSub Channel Id -100xxxxxxxxx
     FSUB = bool(env.get("FSUB", True)) #Keep True If U Want Force Subscribe         
-    SB_PIC = env.get("SB_PIC", "https://envs.sh/N4Z.png") # Add Link For Start Cmd Picture 
+    SB_PIC = env.get("SB_PIC", "https://i.ibb.co/fdfL9Lys/photo-2025-01-31-18-49-57-7466153839201091588.jpg") # Add Link For Start Cmd Picture 
     BOT_USERNAME = env.get("BOT_USERNAME", "Emiting_Auto_Reaction_Bot") # Add Bot Username Without @
     EMOJIS = [
         "👍", "😚", "❤", "🔥", 
